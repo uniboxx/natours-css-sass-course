3090d7427ca9f9b83d75dfffd56ca2dfbb4b3504
uniboxx <unibox@duck.com>
2024-05-17 16:48:23 +0200

    added autoprefixer package and config

--- vite.config.js
+++ vite.config.js
@@ -6,17 +6,7 @@ export default defineConfig({
 
   css: {
     postcss: {
-      plugins: [
-        autoprefixer({
-          overrideBrowserslist: [
-            '> 0.5%',
-            'last 2 versions',
-            'ie > 11',
-            'iOS >= 10',
-            'Android >= 5',
-          ],
-        }),
-      ],
+      plugins: [autoprefixer],
     },
   },
 });
