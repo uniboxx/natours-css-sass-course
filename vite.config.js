e8ddcc3a7a0f69019094cff842071437da13d546
uniboxx <unibox@duck.com>
2024-05-17 15:54:32 +0200

    finished responsive images on scss

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,10 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/natours-css-sass-course/',
+
+  css: {
+    postcss: {
+      plugins: [autoprefixer({})],
+    },
+  },
 });
